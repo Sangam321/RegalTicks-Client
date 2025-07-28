142bfc9510b915887b1c45088e2fa0ae3207915e
Sangam Basnet <sangambasnet1800@gmail.com>
2025-07-28 16:45:19 +0545

    watch progress api

---
+++ src/features/api/watchProgressApi.js
@@ -0,0 +1,45 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const COURSE_PROGRESS_API = "http://localhost:8080/api/v1/progress";
+
+export const watchProgressApi = createApi({
+  reducerPath: "watchProgressApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: COURSE_PROGRESS_API,
+    credentials: "include",
+  }),
+  endpoints: (builder) => ({
+    getWatchProgress: builder.query({
+      query: (watchId) => ({
+        url: `/${watchId}`,
+        method: "GET",
+      }),
+    }),
+    updateWatch_DetailsProgress: builder.mutation({
+      query: ({ watchId, watch_detailsId }) => ({
+        url: `/${watchId}/watch_details/${watch_detailsId}/view`,
+        method: "POST"
+      }),
+    }),
+
+    completeWatch: builder.mutation({
+      query: (watchId) => ({
+        url: `/${watchId}/complete`,
+        method: "POST"
+      })
+    }),
+    inCompleteWatch: builder.mutation({
+      query: (watchId) => ({
+        url: `/${watchId}/incomplete`,
+        method: "POST"
+      })
+    }),
+
+  }),
+});
+export const {
+  useGetWatchProgressQuery,
+  useUpdateWatch_DetailsProgressMutation,
+  useCompleteWatchMutation,
+  useInCompleteWatchMutation
+} = watchProgressApi;
