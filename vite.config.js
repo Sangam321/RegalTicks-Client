b390e6a16cea22db39f1478fad98d9d446c6ffe5
Sangam Basnet <sangambasnet1800@gmail.com>
2025-07-29 21:15:47 +0545

    updated vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
-import react from "@vitejs/plugin-react"
-import path from "path"
-import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react";
+import fs from "fs";
+import path from "path";
+import { defineConfig } from "vite";
+
+// HTTPS configuration
+const httpsOptions = {
+  key: fs.readFileSync("./key.pem"),
+  cert: fs.readFileSync("./cert.pem")
+};
 
 export default defineConfig({
   plugins: [react()],
@@ -9,4 +16,22 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+  server: {
+    port: 5173,
+    https: httpsOptions,
+    strictPort: true,
+    proxy: {
+
+      "/api": {
+        target: "https://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      }
+    }
+  },
+  preview: {
+    port: 5173,
+    https: httpsOptions
+  }
+});
