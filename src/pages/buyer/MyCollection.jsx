751bef8426a88a9e306f34109c7d74c2eff9fb83
Sangam Basnet <sangambasnet1800@gmail.com>
2025-07-29 21:17:06 +0545

    final change for collection

--- src/pages/buyer/MyCollection.jsx
+++ src/pages/buyer/MyCollection.jsx
@@ -7,12 +7,12 @@ const MyCollection = () => {
   const myCollection = data?.user.enrolledWatchs || [];
   return (
     <div className="max-w-4xl mx-auto my-10 px-4 md:px-0">
-      <h1 className="font-bold text-2xl">MY LEARNING</h1>
+      <h1 className="font-bold text-2xl">MY PURCHASE</h1>
       <div className="my-5">
         {isLoading ? (
           <MyCollectionSkeleton />
         ) : myCollection.length === 0 ? (
-          <p>You are not enrolled in any watch.</p>
+          <p>You have not purchase any watch.</p>
         ) : (
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             {myCollection.map((watch, index) => (
